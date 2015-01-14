848dd7d38c2bd76a333e55b5da4e1cc4b7f5470b
Jared Sorge <jared@jsorge.net>
2015-01-14 15:27:31 -0800

    Wired up the master table view

--- ios Takehome/ios Takehome/Controllers/MasterViewController.swift
+++ ios Takehome/ios Takehome/Controllers/MasterViewController.swift
@@ -9,5 +9,43 @@
 import UIKit
 
 class MasterTableViewController: UITableViewController {
+    //MARK: Constants
+    let cellId = "WeatherCell"
     
+    //MARK: Properties
+    var cityWeather = Weather(name: nil, forecast: nil)
+    lazy var dateFormatter: NSDateFormatter = {
+        var formatter = NSDateFormatter()
+        formatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        return formatter
+        }()
+    @IBOutlet var activitySpinner: UIActivityIndicatorView!
+    
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        downloadSeattleWeatherForNumberOfDays(numberOfDays: 5, { (weather) -> Void in
+            NSOperationQueue.mainQueue().addOperationWithBlock({ () -> Void in
+                self.cityWeather = weather
+                self.tableView.reloadData()
+            })
+        })
+    }
+    
+    //MARK: UITableViewDataSource
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cityWeather.forecast?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellId) as UITableViewCell
+        
+        if let forecastArray = cityWeather.forecast {
+            let day = forecastArray[indexPath.row]
+            
+            cell.textLabel?.text = "\(self.dateFormatter.stringFromDate(day.date!))"
+        }
+        
+        return cell
+    }
 }
